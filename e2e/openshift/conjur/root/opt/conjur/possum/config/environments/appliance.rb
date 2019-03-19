7b179989933d793c5441aecc6c85200c324a0fb6
Srdjan Grubor <sgnn7@sgnn7.org>
2019-03-19 11:00:08 -0500

    Moved in Conjur OpenShift image config from container-appliance
    This image is not usable right now but it should have all the major
    pieces that we need.

---
+++ e2e/openshift/conjur/root/opt/conjur/possum/config/environments/appliance.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+load File.expand_path '../production.rb', __FILE__
+require 'rack/remember_uuid'
+
+Rails.application.configure do
+  config.log_level = :info
+  config.middleware.use Rack::RememberUuid
+#  config.audit_socket = '/run/conjur/audit.socket'
+#  config.audit_database ||= 'postgres://:5433/audit'
+end
