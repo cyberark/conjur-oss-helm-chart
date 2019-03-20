ada0ab26ce59e293b3a686059701b5f5f2410521
Srdjan Grubor <sgnn7@sgnn7.org>
2019-03-20 11:39:04 -0500

    Added seedfile param to from-scratch-oc script
    Since we require a seed file for OpenShift deployment, the main loop
    script now has parameter passing to all child scripts.

--- e2e/from-scratch-oc.sh
+++ e2e/from-scratch-oc.sh
@@ -1,12 +1,19 @@
 #!/bin/bash -e
 
+# Parameter parsing and validation
+if [ $# -lt 1 ]; then
+  echo "ERROR! Seedfile arg is required!"
+  echo "Usage: $0 <seed_file>"
+  exit 1
+fi
+
 echo "* Building images..."
-./build-oc-images.sh
+./build-oc-images.sh "$@"
 
 echo "* Tagging and pushing images..."
-./push-oc-images.sh
+./push-oc-images.sh "$@"
 
 echo "* Deploying to OpenShift..."
-./install-conjur-oc.sh
+./install-conjur-oc.sh "$@"
 
 echo "*** DONE ***"
