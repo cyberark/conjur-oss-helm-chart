1573a82588d88c42a4957cf229479d29cf48352c
Robert Pothier <Robert.pothier@cyberark.com>
2021-07-21 15:28:13 -0400

    Update Helm install and update for Openshift (#154)

--- examples/common/2_helm_install_or_upgrade_conjur.sh
+++ examples/common/2_helm_install_or_upgrade_conjur.sh
@@ -29,32 +29,38 @@ fi
 # Helm upgrade. Otherwise, do a Helm install of the Conjur cluster.
 if [ "$(helm list -q -n $CONJUR_NAMESPACE | grep "^$HELM_RELEASE$")" = "$HELM_RELEASE" ]; then
   echo "Helm upgrading existing Conjur cluster. Waiting for upgrade to complete."
-  helm upgrade \
-      -n "$CONJUR_NAMESPACE" \
-      --set account.name="$CONJUR_ACCOUNT" \
-      --set account.create="true" \
-      --set authenticators="authn\,authn-k8s/$AUTHENTICATOR_ID" \
-      --set logLevel="$CONJUR_LOG_LEVEL" \
-      --set service.external.enabled="$CONJUR_LOADBALANCER_SVCS" \
-      --reuse-values \
-      --wait \
-      --timeout 300s \
-      "$HELM_RELEASE" \
-      "../../conjur-oss"
+  args=("upgrade" "--reuse-values" )
 else
   # Helm install a Conjur cluster and create a Conjur account
   echo "Helm installing a Conjur cluster. Waiting for install to complete."
   data_key="$(docker run --rm cyberark/conjur data-key generate)"
-  helm install \
-      -n "$CONJUR_NAMESPACE" \
-      --set dataKey="$data_key" \
-      --set account.name="$CONJUR_ACCOUNT" \
-      --set account.create="true" \
-      --set authenticators="authn\,authn-k8s/$AUTHENTICATOR_ID" \
-      --set logLevel="$CONJUR_LOG_LEVEL" \
-      --set service.external.enabled="$CONJUR_LOADBALANCER_SVCS" \
-      --wait \
-      --timeout 300s \
-      "$HELM_RELEASE" \
-      "../../conjur-oss"
+  args=("install" "--set" "dataKey=$data_key" )
 fi
+
+args+=("-n" "$CONJUR_NAMESPACE" \
+      "--set" "account.name=$CONJUR_ACCOUNT" \
+      "--set" "account.create=true" \
+      "--set" "authenticators=authn\,authn-k8s/$AUTHENTICATOR_ID" \
+      "--set" "logLevel=$CONJUR_LOG_LEVEL" \
+      "--set" "service.external.enabled=$CONJUR_LOADBALANCER_SVCS" \
+      "--wait" \
+      "--timeout" "300s" )
+
+if [[ "$PLATFORM" == "openshift" ]]; then
+  args+=("--set" "image.repository=$IMAGE_REPOSITORY" \
+         "--set" "image.tag=$IMAGE_TAG" \
+         "--set" "nginx.image.repository=$NGINX_REPOSITORY" \
+         "--set" "nginx.image.tag=$NGINX_TAG" \
+         "--set" "postgres.image.repository=$POSTGRES_REPOSITORY" \
+         "--set" "postgres.image.tag=$POSTGRES_TAG" \
+         "--set" "postgres.persistentVolume.create=$POSTGRES_PV_CREATE" \
+         "--set" "rbac.create=true" \
+         "--set" "openshift.enabled=$OPENSHIFT_ENABLED" )
+fi
+
+args+=("$HELM_RELEASE" \
+      "../../conjur-oss")
+
+echo "helm" "${args[@]}"
+
+helm "${args[@]}"
---
+++ examples/openshift/0_export_env_vars.sh
@@ -0,0 +1,53 @@
+# KinD and Helm install options
+export CREATE_KIND_CLUSTER="${CREATE_KIND_CLUSTER:-true}"
+export KIND_CLUSTER_NAME="${KIND_CLUSTER_NAME:-kind}"
+export HELM_INSTALL_CONJUR="${HELM_INSTALL_CONJUR:-true}"
+export HELM_RELEASE="${HELM_RELEASE:-conjur-oss}"
+export CONJUR_NAMESPACE="${CONJUR_NAMESPACE:-conjur-oss}"
+export CONJUR_ACCOUNT="${CONJUR_ACCOUNT:-myConjurAccount}"
+export CONJUR_LOG_LEVEL="${CONJUR_LOG_LEVEL:-info}"
+
+# Basic demo config
+export TEST_APP_DATABASE="${TEST_APP_DATABASE:-postgres}"
+export TEST_APP_NAMESPACE_NAME="${TEST_APP_NAMESPACE_NAME:-app-test}"
+
+# Configuration for Conjur authn-k8s
+export ANNOTATION_BASED_AUTHN="${ANNOTATION_BASED_AUTHN:-true}"
+export AUTHENTICATOR_ID="${AUTHENTICATOR_ID:-my-authenticator-id}"
+
+# Conjur OSS Helm chart specific setting for demo scripts
+export CONJUR_OSS_HELM_INSTALLED="${CONJUR_OSS_HELM_INSTALLED:-true}"
+
+# KinD specific specific setting for demo scripts
+export CONJUR_LOADBALANCER_SVCS="${CONJUR_LOADBALANCER_SVCS:-false}"
+export TEST_APP_LOADBALANCER_SVCS="${TEST_APP_LOADBALANCER_SVCS:-false}"
+
+# Openshift specific setting for demo scripts
+export IMAGE_REPOSITORY="${IMAGE_REPOSITORY:-registry.connect.redhat.com/cyberark/conjur}"
+export IMAGE_TAG="${IMAGE_TAG:-latest}"
+export NGINX_REPOSITORY="${NGINX_REPOSITORY:-registry.connect.redhat.com/cyberark/conjur-nginx}"
+export NGINX_TAG="${NGINX_TAG:-latest}"
+export POSTGRES_REPOSITORY="${POSTGRES_REPOSITORY:-registry.redhat.io/rhscl/postgresql-10-rhel7}"
+export POSTGRES_TAG="${POSTGRES_TAG:-latest}"
+export POSTGRES_PV_CREATE="${POSTGRES_PV_CREATE:-false}"
+export OPENSHIFT_ENABLED="${OPENSHIFT_ENABLED:-true}"
+
+# You can choose to have the scripts create a local, insecure Docker registry
+# for pushing/pulling pod images by exporting the following:
+#     export USE_DOCKER_LOCAL_REGISTRY=true
+# Or you can use a public Docker registry (e.g. DockerHub) by exporting
+# your Docker credentials.
+#
+# These can be configured/customized in customize.env
+export USE_DOCKER_LOCAL_REGISTRY="${USE_DOCKER_LOCAL_REGISTRY:-true}"
+export DOCKER_LOCAL_REGISTRY_NAME="${DOCKER_LOCAL_REGISTRY_NAME:-kind-registry}"
+export DOCKER_LOCAL_REGISTRY_PORT="${DOCKER_LOCAL_REGISTRY_PORT:-5000}"
+export DOCKER_REGISTRY_URL="${DOCKER_REGISTRY_URL:-localhost:${DOCKER_LOCAL_REGISTRY_PORT}}"
+
+
+export DOCKER_REGISTRY_PATH="${DOCKER_REGISTRY_PATH:-${DOCKER_REGISTRY_URL}}"
+if [[ "USE_DOCKER_LOCAL_REGISTRY" == "false" ]]; then
+  check_env_var "DOCKER_USERNAME"
+  check_env_var "DOCKER_PASSWORD"
+  check_env_var "DOCKER_EMAIL"
+fi
